d102fb7cf3d5c9803aad8be1210fbd4cf8604e40
malachieborohoul <malachieborohoul@gmail.com>
2024-03-19 13:29:14 +0100

    Conditionals

---
+++ Conditionals.playground/Contents.swift
@@ -0,0 +1,12 @@
+import Cocoa
+
+let population: Int=20000
+let message: String
+
+if(population < 10000){
+    message="\(population) is a small town"
+}else{
+    message="\(population) is pretty big"
+}
+
+print(message)
