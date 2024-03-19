d6bd872bc3c745312a777fbfc148480e1f2f8052
malachieborohoul <malachieborohoul@gmail.com>
2024-03-19 13:45:20 +0100

    TERNARY

--- Conditionals.playground/Contents.swift
+++ Conditionals.playground/Contents.swift
@@ -2,11 +2,22 @@ import Cocoa
 
 let population: Int=20000
 let message: String
+let hasPostOffice: Bool=true
 
-if(population < 10000){
-    message="\(population) is a small town"
+if population < 10000{
+    //message="\(population) is a small town"
 }else{
-    message="\(population) is pretty big"
+    //message="\(population) is pretty big"
 }
 
+
+
+
+
+if !hasPostOffice{
+    print("Where do we buy stamps")
+}
+
+//TERNARY 
+message=population < 10000 ? "\(population) is a small town": "\(population) is quite big"
 print(message)
