f14bf36323a092fca12faf93f84aaa093d76037f
malachieborohoul <malachieborohoul@gmail.com>
2024-03-20 22:06:11 +0100

    Play with switch

--- Switch.playground/Contents.swift
+++ Switch.playground/Contents.swift
@@ -1,15 +1,18 @@
 import Cocoa
 
-var statusCode:  Int=200
+var statusCode:  Int=17
 
 
 switch statusCode{
-case 200:
-    print("Created")
-    
 case 401:
     print("Unauthorized")
-
+case 400...417:
+    print("Client error \(statusCode) ")
+case 500...505:
+    print("Server error \(statusCode)")
+case let code where code < 100 || code >= 600:
+    print("\(code ) is illegal")
 default:
-    print("None")
+    print("Unknown")
 }
+
