381953b9c104d258a14b26bcf0f27d8ca2d0108c
malachieborohoul <malachieborohoul@gmail.com>
2024-03-23 12:36:52 +0100

    Looping over a range

---
+++ Loops.playground/Contents.swift
@@ -0,0 +1,9 @@
+import Cocoa
+
+var myFirstInt: Int=0
+
+for  i in 1...5{
+    myFirstInt += 1
+    
+    print(i)
+}
--- Switch.playground/Contents.swift
+++ Switch.playground/Contents.swift
@@ -51,8 +51,41 @@ default:
 }
 
 
-let age: Int = 18
+let age: Int = 29
 
-if case 18...35 = age {
+if case 18...35=age, age >= 25, age < 30{
     print("Cool demographic")
 }
+
+switch age{
+case let a where (18...30 ~= a) && (a >= 25) && (a < 30):
+    print("Cool demographic, rent a car")
+default:
+    break
+}
+
+/*let point=(x:1,y:4)
+
+switch point {
+case let q1 where (point.x > 0) && (point.y > 0):
+    print("\(q1) is in quadrant 1")
+    
+case let q2 where (point.x < 0) && (point.y > 0):
+    print("\(q2) is in quadrant 2")
+
+case let q3 where (point.x < 0) && (point.y < 0):
+    print("\(q3) is in quadrant 3")
+    
+case let q4 where (point.x > 0) && (point.y < 0):
+    print("\(q4) is in quadrant 4")
+case (_,0):
+    print("\(point) sits on the x-axis")
+case (0,_):
+    print("\(point) sits on the y-axis")
+    
+default:
+    print("Case not found")
+}
+*/
+
+
