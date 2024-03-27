1b4a28a9924613289de5d4cdfb4275aa4809cfb7
malachieborohoul <malachieborohoul@gmail.com>
2024-03-27 11:17:44 +0100

    String with Unicode Scalars

---
+++ Strings.playground/Contents.swift
@@ -0,0 +1,10 @@
+import Cocoa
+
+
+var greeting = #" Hello, playground Ok damn \n I\"m done with you"#
+
+for c:Character in greeting{
+    print("\(c)")
+}
+let snowman = "\u{2603}"
+print(snowman)
