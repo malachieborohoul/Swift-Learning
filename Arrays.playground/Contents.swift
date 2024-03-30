9393ae44722d429f598b47a7088ba48ed1a42b32
malachieborohoul <malachieborohoul@gmail.com>
2024-03-30 13:41:58 +0100

    An array preinitialized with a fixed number

--- Arrays.playground/Contents.swift
+++ Arrays.playground/Contents.swift
@@ -3,10 +3,37 @@ import Cocoa
 var bucketList: [String]=[]
 bucketList = ["Scilicon Valley"]
 
-bucketList.append("Build a house")
+bucketList.append("Build a house for my dad")
+bucketList.append("Build a house for my mum")
+bucketList.append("Get married with the girl i love")
 
 bucketList.remove(at: 0)
 
+print(bucketList[...1])
+
+bucketList[1] += ". God is good"
+
+
+bucketList[0]="Make my dad proud of me"
+
+bucketList.insert("Have intimacy with GOD",at:0)
+
+
+var newItems = [
+    
+        "Buy the latest macbook",
+        "Buy the leatest IPhone",
+        "Buy the latest IPad"
+    
+]
+
+bucketList += newItems
 print(bucketList)
 
 
+var emptyDoubles: [Double] = []
+var emptyFloats: Array<Float> = Array()
+
+
+// An array preinitialized wih a fixed number
+var digitCounts = Array(repeating: 0, count: 10)
