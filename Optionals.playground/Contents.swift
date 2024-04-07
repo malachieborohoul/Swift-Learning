d4215ed13de7e6b375ddc0956cdfb95b631c92dc
malachieborohoul <malachieborohoul@gmail.com>
2024-04-07 23:56:20 +0100

    Refreshing arrays in swift

--- Optionals.playground/Contents.swift
+++ Optionals.playground/Contents.swift
@@ -8,3 +8,32 @@ if errorCodeString != nil{
     let  theError = errorCodeString!
     print(theError)
 }
+
+
+var fruits: [String] = ["banana","mango", "pear"]
+
+for fruit in fruits{
+    print(fruit)
+}
+
+var matA = [[1,1],[2,2]]
+var matB = [[2,2],[1,1]]
+
+var matC: [[Int]]=[]
+
+
+for i in 0..<matA.count{
+    var row: [Int]=[]
+    for j in 0..<matA[i].count{
+       
+        row.append(matA[i][j]+matB[i][j])
+        
+    }
+    
+    matC.append(row)
+}
+
+
+
+
+
